f3353d231f40f5d34bf4cb93de7de0e2cb36f0fa
Filiberka <Filiberka@DESKTOP-E7MVDB9>
2020-12-11 13:49:10 +0100

    Add project files.

---
+++ Caravan Excercise/Interfaces/ITrekbaar.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Caravan_Excercise.Interfaces
+{
+    interface ITrekbaar
+    {
+        int AantalBanden { get; }
+        int Gewicht { get; }
+    }
+}
---
+++ Caravan Excercise/Interfaces/ITrekker.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Caravan_Excercise.Interfaces
+{
+    interface ITrekker
+    {
+        ITrekbaar Aanhangwagen { get; }
+        int MaximaalTrekGewicht { get; }
+
+        void KoppelAanhangwagen(ITrekbaar aanhangwagen);
+    }
+}
